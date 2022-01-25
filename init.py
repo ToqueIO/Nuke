9e99a74ace6c45458ab59b2ed3b5a0afe05821cf
Josh <dragobyvfx@gmail.com>
2022-01-24 18:52:54 -0800

    remove search replace from the init.py

--- init.py
+++ init.py
@@ -6,13 +6,6 @@ import logging
 
 try:
     logging.info('Attempting to initialize Node Tag')
-    import nodeTag
-    nuke.pluginAddPath(os.path.dirname(nodeTag.__file__).replace('\\', '/'))
-except Exception:
-    logging.error(traceback.format_exc())
-
-try:
-    logging.info('Attempting to initialize Node Tag')
     import searchReplace
     nuke.pluginAddPath(os.path.dirname(searchReplace.__file__).replace('\\', '/'))
 except Exception:
