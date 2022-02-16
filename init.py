fb92ebefb43b6e84f9747274c361d679786f3840
Josh <dragobyvfx@gmail.com>
2022-02-15 23:31:25 -0800

    Updated version to release
    Updated init to properly import nodeTag

--- init.py
+++ init.py
@@ -6,7 +6,7 @@ import logging
 
 try:
     logging.info('Attempting to initialize Node Tag')
-    import searchReplace
-    nuke.pluginAddPath(os.path.dirname(searchReplace.__file__).replace('\\', '/'))
+    import nodeTag
+    nuke.pluginAddPath(os.path.dirname(nodeTag.__file__).replace('\\', '/'))
 except Exception:
     logging.error(traceback.format_exc())
